48c5eb9a3dab5dfce8426b1cb2e538c2945e75d1
rfnozn <197492804+rfnozn@users.noreply.github.com>
2026-02-15 13:36:22 +0100

    Toggle footer content when clicking share button

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const popupContent = document.getElementById("article__popup");
+const footerContent = document.getElementById("article__foot");
+const shareButton = document.getElementById("share-btn");
+const contentInfo = document.getElementById("article__content-info");
+
+shareButton.addEventListener("click", () => {
+  contentInfo.classList.toggle("active");
+  popupContent.classList.toggle("active");
+  if(popupContent.classList.contains("active")) {
+    footerContent.style.backgroundColor = "var(--grey-900)";
+  } else {
+    footerContent.style.backgroundColor = "#fff";
+  }
+});
